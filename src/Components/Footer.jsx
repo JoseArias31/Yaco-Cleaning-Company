615e2de8b888319d517bbc334059cd18e9555aec
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2023-10-05 09:53:28 -0400

    Footer updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -50,7 +50,7 @@ return(
                       <h6 className="text-uppercase fw-bold">Contact</h6>
                       <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
                       <p><i className="fas fa-home mr-3"></i> Toronto, ON.</p>
-                      <p><i className="fas fa-envelope mr-3"></i> info@yacocleaning.com.com</p>
+                      <p><i className="fas fa-envelope mr-3"></i> info@yacocleaning.com</p>
                       <p><i className="fas fa-phone mr-3"></i> +1 647-425-2986</p>
                     </div>
                   </div>
