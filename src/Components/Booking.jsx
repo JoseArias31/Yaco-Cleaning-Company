148ac6a9867d0612ed00b80d9e3d90c16954fe32
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2024-12-11 11:05:48 -0500

    Starting Booking section

---
+++ src/Components/Booking.jsx
@@ -0,0 +1,30 @@
+
+
+const Booking = () =>{
+    
+    const cleaners = [
+        {
+            name: 'Maria',
+            photo:'Ma'
+        },{
+            name: 'Marisol',
+            photo:'Ma'
+        },{
+            name: 'Andrea',
+            photo:'Ma'
+        },
+    ];
+
+    return (
+
+       <div>
+        <h1>Booking your Cleaning Appointment</h1>
+        <h2>Book an Appoitment with Maria</h2>
+        <h2>Book an Appoitment with Marisol</h2>
+        <h2>Book an Appoitment with Andrea</h2>
+        <img></img>
+        
+       </div>
+    )
+}
+export default Booking;
